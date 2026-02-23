2568e38703cf447f8b2640df71752756bf0e96ea
raymon rutten <11116196@student.hhs.nl>
2026-02-23 09:20:59 +0100

    Adding resources

--- .godot/mono/temp/obj/Debug/GameTools.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/GameTools.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameTools")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5334db19f0767ceb38f6407ddde349e2db9cd4ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fdfcf8d97e5199280346184d13bc5775e84dd2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameTools")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameTools")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
